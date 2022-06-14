2e9a0720cb824d340f0da36685406de84f771fbb
BillyA15 <61017717+BillyA15@users.noreply.github.com>
2022-06-14 19:28:15 +0300

    added file collision support, creation of folders

--- src/Order.py
+++ src/Order.py
@@ -5,6 +5,7 @@ from turtle import clear
 #import regular expressions
 import re
 #enum with 3 states
+import shutil
 
 from colorama import Fore #for color
 
@@ -112,9 +113,13 @@ def move_dfs(dictionary, current_name,new_name):
             #dictionary[new_name+"/"+value[1]] = dictionary.pop(current_name+"/"+value[1])
 
 def remove_folder(dictionary, parent_dir, current_folder):
-
+    #check if it does not exist and return if it does not
+    if parent_dir+'/'+current_folder not in dictionary:
+        print("ERROR: "+ os.path.join(parent_dir,current_folder)+" does not exist")
+        return
     #remove folder from operating system
-    os.rmdir(os.path.join(parent_dir,current_folder))
+    ##os.rmdir(os.path.join(parent_dir,current_folder))
+    shutil.rmtree(os.path.join(parent_dir,current_folder))
 
     dictionary[parent_dir].remove([FOLDER,current_folder])
     remove_dfs(dictionary, parent_dir+"/"+current_folder)
@@ -132,6 +137,46 @@ def remove_dfs(dictionary, current_name):
     
     dictionary.pop(current_name)
 
+def add_folder(dictionary, parent_dir, current_folder, list_of_lists):
+    #check if folder exists in dictionary and return if it does
+    if parent_dir+'/'+current_folder in dictionary:
+        print("ERROR: "+ parent_dir+'/'+current_folder + " already exists")
+        return
+
+    #create folder in operating system
+    os_dir = os.path.join(*parent_dir.split('/'),current_folder)
+    os.makedirs(os.path.join(os_dir))
+
+    #add the new folder to the parent directory for reference
+    dictionary[parent_dir].append([FOLDER, current_folder])
+
+    #create the empty folder in the dictionary
+    dictionary[parent_dir+"/"+current_folder] = []
+
+    #for each item in the list of lists, add it to the dictionary
+    for value in list_of_lists:
+        if value[0] == FOLDER:
+            #add the folder under the new folder
+            dictionary[parent_dir+"/"+current_folder].append([FOLDER, value[1]])
+            #create the empty folder entry for the dictionary
+            dictionary[parent_dir+"/"+current_folder+"/"+value[1]] = []
+        elif value[0] == EXTENSION:
+            #add the extension to the dictionary list of the new folder
+            dictionary[parent_dir+"/"+current_folder].append([EXTENSION, value[1]])
+        elif value[0] == REGULAR_EXPRESSION:
+            #add the regex to the dictionary list of the new folder
+            dictionary[parent_dir+"/"+current_folder].append([REGULAR_EXPRESSION, value[1]])
+        else:
+            print("ERROR: unknown type") # ERROR all CAPS
+    
+    first_pair = next(iter((dictionary.items())))
+
+    #update extensions
+    #first pair is the root
+    dfs(config,first_pair[0])
+
+
+
 #read from json file
 def read_json(file_name):
     with open(file_name, 'r') as f:
@@ -142,6 +187,29 @@ def read_json(file_name):
 def write_json(file_name, data):
     with open(file_name, 'w') as f:
         json.dump(data, f, indent=4)
+
+def handle_file_collision(root,file):
+    while os.path.isfile(os.path.join(root,file)):
+                        #get the file name and extension
+        file_name = os.path.splitext(file)[0]
+        file_extension = os.path.splitext(file)[1]
+        #get the number in parenthesis
+        number = re.search(r'\((\d+)\)', file_name)
+        if number:
+            #get the number
+            number = number.group(1)
+            #remove the number in parenthesis
+            file_name = file_name.replace('('+number+')','')
+            #increase the number by 1
+            number = int(number) + 1
+            #add the number in parenthesis
+            file_name = file_name + '(' + str(number) + ')' + file_extension
+        else:
+            #add the number in parenthesis
+            file_name = file_name + '(1)' + file_extension
+        file = file_name
+    return file
+
 #scan a directory and check the extension of each file
 def scan_directory(directory):
     for root, dirs, files in os.walk(directory):
@@ -150,7 +218,11 @@ def scan_directory(directory):
             for reg in regex:
                 if re.search(reg, file):
                     #move file to the folder
-                    os.rename(os.path.join(root,file), os.path.join(regex[reg],file))
+                    
+                    new_file_name = handle_file_collision(regex[reg],file)
+                   
+                    os.rename(os.path.join(root,file), os.path.join(regex[reg],new_file_name))
+                  
                     cont_loop = True
                     break
             if cont_loop:
@@ -159,7 +231,9 @@ def scan_directory(directory):
             #check if file extension is in the list of extensions
             if "."+file.split('.')[-1] in extensions:
                 #move file to the folder
-                os.rename(os.path.join(root,file), os.path.join(extensions['.'+file.split('.')[-1]],file))
+                new_file_name = handle_file_collision(extensions['.'+file.split('.')[-1]],file)
+
+                os.rename(os.path.join(root,file), os.path.join(extensions['.'+file.split('.')[-1]],new_file_name))
 
 if __name__ == '__main__':
   
@@ -175,6 +249,8 @@ if __name__ == '__main__':
     #move_folder(config, "Root/Documents", "PDF", "Root/Documents/WORD", "PDFS")
     ##move_folder(config, "Root/Documents", "PDF", "Root/Documents/WORD", "PDFS")
     #move_folder(config, "Root", "Documents", "Root/Documents", "PDF")
+    ###add_folder(config,"Root","NEW_FOLDER",[[FOLDER,"NEW_FOLDER_2"],[EXTENSION,".txt"]])
+    ###remove_folder(config,"Root","NEW_FOLDER")
     '''
     rename_folder(config, None , "Root", "New Root")
     rename_folder(config, "New Root" ,"Documents", "Doc")
